8991d1f848b53632369d24e8afe1f768c272b810
Netravati <Netravati.Hiremath@Ltts.com>
2022-04-12 11:37:05 +0530

    ClientLibrary_NugetPackage

---
+++ ClassLibrary_NugetPackage/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary_NugetPackage
+{
+    public class Class1
+    {
+        public void Log(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+}
